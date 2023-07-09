c1d9d516a2bdaa0c23de256d04b39e2268367a67
Athichart Tangpong <victorybattleship@gmail.com>
2023-07-08 20:50:51 -0700

    [filelib.py] Remove obsolete code

--- scripts/obsolete/filelib.py
+++ scripts/obsolete/filelib.py
@@ -10,23 +10,6 @@ from debuglib import t0, t1, t2, pprint1
 
 junkFileList = [ '.DS_Store', '.wdmc', '.fseventsd', '.Spotlight-V100' ]
 
-def deleteJunkFiles( filename, moreJunkFileList=None ):
-    '''Delete junk files (mac os). Return True if deleted; otherwise, False'''
-    global junkFileList
-    
-    if moreJunkFileList:
-        junkFileList += moreJunkFileList 
-        
-    f = os.path.basename( filename )
-    if f in junkFileList:
-        if os.path.isdir( filename ):
-            shutil.rmtree( filename )
-        else:
-            os.remove( filename )
-        t1( "%s removed %s" % ( deleteJunkFiles.__name__, filename ) )
-        return True
-    return False
-
 def folderExists( path ):  
     '''Check if path exists and is a folder, not file'''
     if not os.path.exists( path ) or not os.path.isdir( path ):
